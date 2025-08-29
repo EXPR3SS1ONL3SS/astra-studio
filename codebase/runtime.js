ed72cda02c09d6f2e4acdd49f0dcd6b2d0671a58
Master Tengen 💯 <219903610+EXPR3SS1ONL3SS@users.noreply.github.com>
2025-08-29 12:24:36 +0000

    e

---
+++ codebase/runtime.js
@@ -0,0 +1,8 @@
+const ts = require("typescript");
+var fs = require('node:fs')
+var path = require('node:path')
+
+let file_path = "./ts"
+
+let file_reader = new FileReader()
+file_reader.readAsDataURL
