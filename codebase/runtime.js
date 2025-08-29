69df75e5889dc69686270e0ff2034aa1dd1655da
Master Tengen 💯 <219903610+EXPR3SS1ONL3SS@users.noreply.github.com>
2025-08-29 12:54:27 +0000

    t

--- codebase/runtime.js
+++ codebase/runtime.js
@@ -1,8 +1,47 @@
 const ts = require("typescript");
-var fs = require('node:fs')
-var path = require('node:path')
+const fs = require('node:fs');
+const path = require("node:path");
+const { computeCommonSourceDirectoryOfFilenames } = require("./typescript_compiler");
 
-let file_path = "./ts"
+function read(dir="./ts",
+    callback = (err, files) => {
+        if (err) {console.warn("Error: " + err); return}
+        for (const file of files) {
+        
+            console.log(file)
+            fs.stat(path.join(dir, file), (sErr, stats) => {
+                if (sErr) {
+                    console.warn("Error: " + sErr)
+                    return
+                }
+                if (stats.isFile()) {
+                    fs.readFile(file, {
+                        "encoding": "utf-8"
+                    }, (rErr, data) => {
+                        if (err) {
+                            console.warn("Error reading file: " + rErr)
+                            return
+                        }
+                        return data
+                    })
+                } else if (stats.isDirectory()) {
+                    
+                }
+            })
+        }
+    }
+) {
+    let source = ""
+    source = fs.readdir()
+}
 
-let file_reader = new FileReader()
-file_reader.readAsDataURL
+fs.readdir(file_path, (err, files) => {
+    if (err) {
+        console.warn("Error: " + err)
+        return
+    }
+    for (const file of files) {
+        fs.stat(path.join(file_path, file), (sErr))
+    }
+    read_async()
+})
