a63c4cb5fca0a0c129681b37da267273fdecfe88
Master Tengen 💯 <219903610+EXPR3SS1ONL3SS@users.noreply.github.com>
2025-09-09 12:54:57 +0000

    push all updates

--- codebase/runtime.js
+++ codebase/runtime.js
@@ -3,19 +3,29 @@ const fs = require('fs/promises')
 const path = require('path')
 const {Buffer} = require('buffer')
 const vm = require('vm')
+let ts_config = await fetch("https://raw.githubusercontent.com/EXPR3SS1ONL3SS/astra-studio/refs/heads/main/tsconfig.json?token=GHSAT0AAAAAADK2DOLPKJARXFIP5XWX6A522GAGFLQ").response().json()
 /**
  * 
  * @param {string} source The path of what you want to compile. If it is a directory but directory is false, it will NOT compile.
  * likewise, if it is a file and directory is true, it won't compile either.
  * Automatically sets up the file structing,
  * @param {boolean} directory Determines whether or not the source is a directory. must match to function properly.
+ * @returns {string|string[]}
  */
 function convert(source, directory=false) {
+    let source_str = ""
+    let source_map = []
     if (directory) {
-        fs.readdir(source, {
-            "recursive": true,
-        }).then((stuff) => {
-
+        fs.readdir(source).then((stuff) => {
+            for (const file of stuff) {
+                let stats = fs.stat(file).then((stats) => {
+                    if (stats.isFile()) {
+                        source_map.push(ts.transpile(file, ts_config.compilerOptions || {}))
+                    } else if (stats.isDirectory()) {
+                        source_map.push({path:file, sources:convert(file, true)})
+                    }
+                }).catch(e => console.log(e))
+            }
         }).catch((e) => {
             
         })
--- codebase/ts/types.ts
+++ codebase/ts/types.ts
@@ -1,13 +1,3 @@
 export type dict = Record<string, any>
 
 export type fn = (...args: any[]) => any
-
-function func(x:number, y: string): number {
-    return x + Number(y)
-}
-
-let type1 = typeof func(1,"2")
-let type2 = typeof(func(1,"2"))
-
-console.log(type1)
-console.log(type2)
